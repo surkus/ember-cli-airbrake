e4d15b9fbb0a2cc04a32d4285f5d54e5f09edca3
Larry Sprock <larry@lucidbleu.com>
2019-01-25 08:45:38 -0800

    Remove sourcemap line from airbrake-js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const Funnel = require('broccoli-funnel');
 const mergeTrees = require('broccoli-merge-trees');
 const resolve = require('resolve');
+const fs = require('fs');
 
 module.exports = {
   name: require('./package').name,
@@ -11,14 +12,17 @@ module.exports = {
   included: function() {
     this._super.included.apply(this, arguments);
 
-    let vendorPath = `vendor/${this.name}`;
-    let host = this._findHost();
+    const vendorPath = `vendor/${this.name}`;
+    const host = this._findHost();
+    const filePath = path.join(vendorPath, 'client.js');
 
-    host.import(path.join(vendorPath, 'client.js'));
+    host.import(filePath);
   },
 
   treeForVendor() {
     let airbrakePath = path.join(this.resolvePackagePath('airbrake-js'), 'dist');
+
+    this._removeRefToSourceMap(airbrakePath);
 
     let airbrakeJs = new Funnel(airbrakePath, {
       files: ['client.js'],
@@ -31,6 +35,17 @@ module.exports = {
   resolvePackagePath(packageName) {
     let host = this._findHost();
     return path.dirname(resolve.sync(`${packageName}/package.json`, { basedir: host.project.root }));
+  },
+
+  async _removeRefToSourceMap(jsPath) {
+    const filePath = path.join(jsPath, 'client.js')
+
+    await fs.readFile(filePath, 'utf8', function(err, data) {
+      let file = data.split('\n');
+
+      file.pop();
+      fs.writeFileSync(filePath, file.join('\n'));
+    });
   },
 
   _ensureFindHost() {
